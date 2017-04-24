e10f1071b4a9fe3b5ba389c17b4ae629678aff97
jorge0907 <jorge_sr95@hotmail.com>
2017-04-24 17:56:00 -0500

    avances
    varios avances

--- Hospital/Controllers/EspecialidadController.cs
+++ Hospital/Controllers/EspecialidadController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Hospital.Models;
 
 namespace Hospital.Controllers
 {
@@ -12,6 +13,49 @@ namespace Hospital.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult obtenerEspecialidad(int id)
+        {
+            Entities model = new Entities();
+            var dato = (from e in model.ESPECIALIDAD where e.ID == id select e).First();
+            return Json(dato);
+        }
+
+        public ActionResult obtenerEspecialidades(string nombre)
+        {
+            Entities model = new Entities();
+            if (nombre == "")
+            {
+                var datos = (from e in model.ESPECIALIDAD where e.VISIBLE == true select e).ToList();
+                return Json(datos);
+            }
+            else
+            {
+                var datos = (from e in model.ESPECIALIDAD where e.NOMBRE.Contains(nombre) && e.VISIBLE == true select e).ToList();
+                return Json(datos);
+            }
+        }
+
+        public ActionResult agregarEspecialidad(string nombre)
+        {
+            Entities model = new Entities();
+            ESPECIALIDAD especialidad = new ESPECIALIDAD();
+            especialidad.NOMBRE = nombre;
+            especialidad.VISIBLE = true;
+            model.ESPECIALIDAD.Add(especialidad);
+            model.SaveChanges();
+            return Json(especialidad);
+        }
+
+        public ActionResult modificarEspecialidad(int id, string nombre, bool visible)
+        {
+            Entities model = new Entities();
+            var especialidad = (from e in model.ESPECIALIDAD where e.ID == id select e).First();
+            especialidad.NOMBRE = nombre;
+            especialidad.VISIBLE = visible;
+            model.SaveChanges();
+            return Json(especialidad);
         }
     }
 }
