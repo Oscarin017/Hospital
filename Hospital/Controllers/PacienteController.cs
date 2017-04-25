80f552b128262a525d689d1d629e11141047e080
emersonmeraz <merfrayemerson14@gmail.com>
2017-04-24 19:42:17 -0500

    Paciente
    Pantalla paciente.

--- Hospital/Controllers/PacienteController.cs
+++ Hospital/Controllers/PacienteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hospital.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,65 @@ namespace Hospital.Controllers
 {
     public class PacienteController : Controller
     {
-        // GET: Paciente
+        // GET: Doctor
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult obtenerPaciente(int id)
+        {
+            Entities model = new Entities();
+            var dato = (from m in model.PACIENTE where m.ID == id select m).First();
+            return Json(dato);
+        }
+
+        public ActionResult obtenerPacientes(string nombre)
+        {
+            Entities model = new Entities();
+            if (nombre == "")
+            {
+                var datos = (from m in model.PACIENTE where m.VISIBLE == true select m).ToList();
+                return Json(datos);
+            }
+            else
+            {
+                var datos = (from m in model.PACIENTE where m.NOMBRE.Contains(nombre) && m.VISIBLE == true select m).ToList();
+                return Json(datos);
+            }
+        }
+
+        public ActionResult agregarPaciente(string nombre, string apellido, DateTime fechaNacimiento, string sexo, string direccion, string telefono, string correo)
+        {
+            Entities model = new Entities();
+            PACIENTE paciente = new PACIENTE();
+            paciente.NOMBRE = nombre;
+            paciente.APELLIDO = apellido;
+            paciente.CORREO_ELECTRONICO = correo;
+            paciente.FECHA_NACIMIENTO = fechaNacimiento;
+            paciente.SEXO = sexo;
+            paciente.DIRECCION = direccion;
+            paciente.TELEFONO = telefono;
+            paciente.VISIBLE = true;
+            model.PACIENTE.Add(paciente);
+            model.SaveChanges();
+            return Json(paciente);
+        }
+
+        public ActionResult modificarPaciente(int id, string nombre, string apellido, DateTime fechaNacimiento, string sexo, string direccion, string telefono, string correo, bool visible)
+        {
+            Entities model = new Entities();
+            var paciente = (from m in model.PACIENTE where m.ID == id select m).First();
+            paciente.NOMBRE = nombre;
+            paciente.APELLIDO = apellido;
+            paciente.CORREO_ELECTRONICO = correo;
+            paciente.FECHA_NACIMIENTO = fechaNacimiento;
+            paciente.SEXO = sexo;
+            paciente.DIRECCION = direccion;
+            paciente.TELEFONO = telefono;
+            paciente.VISIBLE = visible;
+            model.SaveChanges();
+            return Json(paciente);
         }
     }
 }
